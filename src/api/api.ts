d383d2c5389c3e2650b3285da780d067daf0cad1
rainswift <837484536@qq.com>
2022-11-24 00:39:41 +0800

    提交

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@
 import request from '@/utils/request'
 
 export function editSave({data}: { data: any }) {
+    console.log(data)
     return request({
         url: 'http://localhost:8080/editSave',
         method: 'post',
--- src/views/edit.vue
+++ src/views/edit.vue
@@ -7,6 +7,7 @@ import { ref } from 'vue';
 import {editSave}from '@/api/api'
 import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
+import request from '@/utils/request'
 
 const text = ref('Hello Editor!');
 let html =''
@@ -14,11 +15,17 @@ let html =''
 const save = ((e)=>{
   let obj = {
     title:'cs',
-    content:html
+    content:html,
+    imgBg:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-6056f138-61fb-4f09-bc22-b277059a45e3/55d7f93c-3dcd-4de8-9472-596903311a86.jpeg',
+    label:'实用教程',
   }
-  editSave((obj)=>{
+  request.post('http://localhost:8080/editSave',obj).then(res => {
+	  
+	  console.log(res.data)
+	})
+  // editSave((obj)=>{
 
-  })
+  // })
 	console.log(e)
 })
 const saveHtml = ((e)=>{
